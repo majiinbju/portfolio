45b3f5d5f8532e4142146945af28f5bb3375f69b
vivek <vivekbajaj14@gmail.com>
2022-09-28 01:42:03 +0530

    added site content

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,11 +4,8 @@
 <?php use Kirby\Text\SmartyPants; snippet('header') ?>
 
 <main class="main-grid">
-	<p class="about text">
-		<?= $page->about() ?>
-	</p>
-	<p class="about-links text">
-		<?= $page->links() ?>
+	<p class="about-text">
+		<?= $page->text() ?>
 	</p>
 </main>
 
