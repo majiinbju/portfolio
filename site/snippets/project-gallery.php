cbb664e963cd8a7af00d4316a53b244a128e1001
Vivek <vivekbajaj14@gmail.com>
2023-07-18 21:04:04 +0530

    project gallery update
    updated headers to look better

--- site/snippets/project-gallery.php
+++ site/snippets/project-gallery.php
@@ -15,13 +15,13 @@
 
     <div class="d-flex flex-column align-items-center col-md-6 justify-content-md-end">
       <?php if ($image->heading()->isNotEmpty()) : ?>
-      <strong class="align-self-start pt-5 pb-0 me-md-5 subheading ">
+      <strong class="align-self-start pt-5 pb-0 me-md-1 subheading ">
         <?= $image->heading()->smartypants() ?>
       </strong>
       <?php endif ?>
 
       <?php if ($image->caption()->isNotEmpty()) : ?>
-      <p class="pt-2 me-md-5 caption">
+      <p class="pt-2 me-md-1 caption">
         <?= $image->caption()->smartypants() ?>
       </p>
       <?php endif ?>
--- site/snippets/project-info.php
+++ site/snippets/project-info.php
@@ -8,6 +8,11 @@
     <?php snippet("project-image"); ?>
   </div>
   <!-- summary -->
+  <?php if ($page->image()->heading()->isNotEmpty()) : ?>
+  <strong class="align-self-start pt-5 pb-0 me-md-1 subheading text-md-center">
+    <?= $page->image()->heading()->smartypants() ?>
+  </strong>
+  <?php endif ?>
   <p class="summary w-100 p-4 mt-2 pb-0 text-md-center">
     <?= $page->summary()->smartypants(); ?>
   </p>
